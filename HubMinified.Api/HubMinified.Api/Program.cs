8cf8fef74340265dada9c4bf0500abfd189f4f0d
Arthur Santana <arthurgn.santana@gmail.com>
2025-05-10 11:29:29 -0300

    Add gRPC service configuration and update Kestrel settings in Program.cs

--- HubMinified.Api/HubMinified.Api/Program.cs
+++ HubMinified.Api/HubMinified.Api/Program.cs
@@ -1,6 +1,8 @@
 using System.Text.Json.Serialization;
 using HubMinified.Api.Filters;
+using HubMinified.Application.Grpc;
 using HubMinified.IoC;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
 DependencyContainer.RegisterServices(builder.Services, builder.Configuration);
 
 builder.Services.AddControllers();
+builder.Services.AddGrpc();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>().AddProblemDetails();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Setup a HTTP/2 endpoint without TLS for development
+    options.ListenLocalhost(5001, o => o.Protocols = HttpProtocols.Http2);
+    
+    // Setup a HTTP/1.1 and HTTP/2 endpoint with TLS for production
+    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http1);
+});
 
 var app = builder.Build();
 
@@ -28,5 +40,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGrpcService<FreightGrpcService>();
 
 app.Run();
